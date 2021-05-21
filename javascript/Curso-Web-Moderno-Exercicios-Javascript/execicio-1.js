b19b41dc9c92aaef57eb1ce00c524ad77e754ba8
Cristiano Souza <cristiano.souza@docspider.com.br>
2021-05-21 13:31:11 -0300

    Primeiro exercicio

---
+++ javascript/Curso-Web-Moderno-Exercicios-Javascript/execicio-1.js
@@ -0,0 +1,8 @@
+function calcular(valor1, valor2) {
+    console.log(valor1 + valor2)
+    console.log(valor1 - valor2)
+    console.log(valor1 * valor2)
+    console.log(valor1 / valor2)
+}
+
+calcular(20, 2)
