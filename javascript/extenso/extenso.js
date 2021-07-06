ae239229f6d0f1bc9a62578946ce41c4627ce40d
Cristiano Souza <cristiano.souza@docspider.com.br>
2021-07-05 23:31:57 -0300

    Parcial, erro em valores grandes

--- javascript/extenso/extenso.js
+++ javascript/extenso/extenso.js
@@ -1,28 +1,105 @@
-const casas = valor => Array.from(valor.toString())
-
-const grupo = ['', 'mil', 'milhoes', 'bilhoes', 'trilhões']
-
-function unidade (digito, indice)
+function extenso(valor)
 {
-    return {
-        digito: digito,
-        posicao: 2 - indice % 3,
-        grupo: grupo[Math.floor(indice / 3)]
+    const unidades = 
+    [
+        "zero",
+        "um", 
+        "dois", 
+        "três",
+        "quatro",
+        "cinco",
+        "seis",
+        "sete",
+        "oito",
+        "nove",
+    ]
+
+    const primeirasDezena =
+    {
+        10: "dez",
+        11: "onze",
+        12: "doze",
+        13: "treze",
+        14: "quatorze",
+        15: "quinze",
+        16: "dezesseis",
+        17: "dezessete",
+        18: "dezoito",
+        19: "dezenove",
     }
+
+    const demaisDezenas =
+    {
+        2: "vinte",
+        3: "trinta",
+        4: "quarenta",
+        5: "cinquenta",
+        6: "sessenta",
+        7: "setenta",
+        8: "oitenta",
+        9: "noventa",
+    }
+
+    const centenas =
+    {
+        0: "",
+        1: "cento",
+        2: "duzentos",
+        3: "trezentos",
+        4: "quatrocentos",
+        5: "quinhentos",
+        6: "seiscentos",
+        7: "setecentos",
+        8: "oitocentos",
+        9: "novecentos",
+    }
+
+    const valoresGrandes = [
+
+        { valor: 1000000000000, nome: "trilhões" },
+        { valor: 1000000000, nome: "bilhões"},
+        { valor: 1000000, nome: "milhões"},
+        { valor: 1000, nome: "mil"}
+    ]
+
+    const e = (a, b) => (!!a && !! b && b != unidades[0]) 
+        ? a + " e " + b
+        : (!!a?a:b)
+
+
+    const separarBloco = (valor, de, ate) => Math.trunc(valor / de) % ate
+    const centena = valor => separarBloco(valor, 100, 1000)
+    const dezena = valor => separarBloco(valor, 10, 100)
+    const unidade = valor => valor % 10
+
+    const extensoDez = (valor) => dezena(valor) == 1
+        ? primeirasDezena[valor]
+        : e(demaisDezenas[dezena(valor)], unidades[unidade(valor)]);
+
+    const vazioSeZero = (setenca) => setenca == unidades[0]
+        ? ""
+        : setenca
+    
+    const aplicarFuncaoNaParcial = (valor, funcao) => vazioSeZero(funcao(valor))
+
+    const extensoCentena = valor => valor == 100 
+        ? "cem"
+        : e(centenas[centena(valor)], aplicarFuncaoNaParcial(valor % 100, extensoDez))
+    
+    const seTemNomeia = (valor, nome) => valor != "" 
+        ? ""
+        : valor + " " + nome
+
+    const divideBlocos = (valor, indiceGrandesValores) => valor < 1000
+        ? extensoCentena(valor)
+        : e(seTemNomeia(separarBloco(valor, valoresGrandes[indiceGrandesValores].valor, valoresGrandes[indiceGrandesValores].valor * 1000), valoresGrandes[indiceGrandesValores].nome)
+        , divideBlocos(valor % valoresGrandes[indiceGrandesValores].valor, indiceGrandesValores + 1))
+
+    const casoZero = valor => valor == 0 
+        ? unidades[0]
+        : divideBlocos(valor, 0)
+    
+    return casoZero(valor)
 }
 
-const agrupar = (lista, item) => lista 
-? lista[item.grupo].concat(item)
-: lista = [item.grupo]
-
-
-
-const extenso = valor => casas(valor)
-.reverse()
-.map(unidade)
-.reduce((a, x) => ({...a, [x.grupo] : {...[x.grupo], x}}), {})
-
-
-
-
-console.log(extenso(123456789))
+module.exports = extenso
