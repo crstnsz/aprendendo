b90dcb051a9eb597e69886446dd1466bd7c3d76a
Cristiano Souza  - Docspider Software S.A <cristianos@docspider.com.br>
2021-09-02 21:39:49 -0300

    Novas tentativas

---
+++ c#/funcional-2/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+using static System.Convert;
+
+var valor = ReadLine();
+
+var numero = ToInt32(valor);
+
+WriteLine(Fatorial(numero));
+
+static int Fatorial(int numero) => numero switch 
+{
+    0 => 1,
+    1 => 1,
+    2 => 2,
+    _ => numero * Fatorial(numero - 1)
+};
