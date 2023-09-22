677b507c2fc0c7c38b128e6d7ab8d3fcb7d9ce29
Cristiano Souza <cristianos@docspider.com.br>
2023-09-22 14:25:08 -0300

    Novos itens

--- c#/csharp-scripts/teste.csx
+++ c#/csharp-scripts/teste.csx
@@ -1,1 +1,5 @@
 Console.WriteLine("Olá mundo");
+
+
+for(int i = 0; i < 1000; i++)
+    Console.WriteLine($"Olá mundo {i}");
--- c#/language-ext-1/Program.cs
+++ c#/language-ext-1/Program.cs
@@ -1,16 +1,52 @@
-﻿using static LanguageExt.Prelude;
+﻿#nullable enable
+
+using static LanguageExt.Prelude;
 using LanguageExt;
 
-var a = Optional(1)
-    .MatchUnsafe(x => x, {
-        Console.WriteLine("Call none");
-        return 2;
-    });
-    
+namespace Teste;
 
-int? i = null;
-var b = Optional(i)
-    .MatchUnsafe(x => x, 2);
-    
-Console.WriteLine($"{a} - {b}");
+public class Program
+{
+
+    public static int Main(string[] args)
+    {
+        var a = Optional(1)
+        .MatchUnsafe(x => x, () => {
+            Console.WriteLine("Call none");
+            return 2;
+        });
+        
+
+    int? i = null;
+    var b = Optional(i)
+        .MatchUnsafe(x => x, 2);
+        
+    Console.WriteLine($"{a} - {b}");
+
+    RC[] rs = new RC[]
+    {
+        new RC(null),
+        new RC(new RB(null)),
+        new RC(new RB(new RA("X")))
+    };
+
+        foreach (var r in rs)
+            Console.WriteLine(r
+        .Apply(Prelude.Optional)
+        .Match(x => x.RB, Prelude.None)
+        .Match(
+            .Map(x => x.RB.Apply(Prelude.Optional)
+                .Map(x => x.RA.Apply(Prelude.Optional)
+                .Map(x => x.A))))
+        .MatchUnsafe(x => x, "nops"));
+
+        return 1;
+    }
+}
+
+
+
+record RA(string? A);
+record RB(RA?  RA);
+record RC(RB? RB);
 
