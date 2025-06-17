bcf9da6928ad2ae1656da461b244e410c7216832
crstnsz <crstnsz@gmail.com>
2025-06-17 00:01:27 -0300

    RabbitMQ fanout

---
+++ rabbitmq/fanout-csharp/consumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(exchange: "my-logs",
+    type: ExchangeType.Fanout);
+
+// declare a server-named queue
+QueueDeclareOk queueDeclareResult = await channel.QueueDeclareAsync();
+string queueName = queueDeclareResult.QueueName;
+await channel.QueueBindAsync(
+        queue: queueName,
+        exchange: "my-logs",
+        routingKey: string.Empty);
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += (model, ea) =>
+{
+    byte[] body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine(message);
+    return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(queueName, autoAck: true, consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ rabbitmq/fanout-csharp/producer/Program.cs
@@ -0,0 +1,27 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(
+        exchange: "my-logs", 
+        type: ExchangeType.Fanout);
+
+string[] messages = 
+[
+    "log 1",
+    "log 2",
+    "log 3"
+];
+
+foreach(var log in messages)
+{
+    var body = Encoding.UTF8.GetBytes(log);
+
+    await channel.BasicPublishAsync(
+            exchange: "my-logs",
+            routingKey: string.Empty,
+            body: body);
+}
