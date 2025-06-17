f37ccb2548123a130194a8bd189763abad8b764e
crstnsz <crstnsz@gmail.com>
2025-06-16 23:24:56 -0300

    rabbit mq + hope

---
+++ rabbitmq/simple-csharp/consumer/Program.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "my_queue", durable: true, exclusive: false,
+    autoDelete: false, arguments: null);
+
+await channel.BasicQosAsync(prefetchSize: 0, prefetchCount: 1, global: false);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+var consumer = new AsyncEventingBasicConsumer(channel);
+consumer.ReceivedAsync += async (model, ea) =>
+{
+    byte[] body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+
+    // here channel could also be accessed as ((AsyncEventingBasicConsumer)sender).Channel
+    await channel.BasicAckAsync(deliveryTag: ea.DeliveryTag, multiple: false);
+};
+
+await channel.BasicConsumeAsync("my_queue", autoAck: false, consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ rabbitmq/simple-csharp/producer/Program.cs
@@ -0,0 +1,32 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory
+{
+    Uri = new Uri("amqp://localhost")
+};
+
+using var connection = await factory.CreateConnectionAsync();
+using var channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "my_queue", durable: true, exclusive: false,
+    autoDelete: false, arguments: null);
+
+string[] messages = [
+    "message 1",
+    "hello",
+    "data to send"
+];
+
+
+foreach(var message in messages)
+{
+    var body = Encoding.UTF8.GetBytes(message);
+
+    await channel.BasicPublishAsync(
+            exchange: string.Empty,
+            routingKey: "my_queue",
+            mandatory: true,
+            basicProperties: new BasicProperties() { Persistent = true }, 
+            body: body);
+}
